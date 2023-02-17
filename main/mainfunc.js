41237174c77799f2a7032b55e04aa41f0d29dd6d
E73707 <94885681+E73707@users.noreply.github.com>
2023-02-17 23:04:24 +1100

    fixed issue where databases werent working, added functionality for view roles, department, empployees and added functionality for adddepartment and add roles

--- main/mainfunc.js
+++ main/mainfunc.js
@@ -2,6 +2,8 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const cTable = require("console.table");
 const { up } = require("inquirer/lib/utils/readline");
+const { type } = require("os");
+const { number } = require("easy-table");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -28,15 +30,6 @@ const menu = [
   },
 ];
 
-// connection.connect((err) => {
-//   if (err) {
-//     console.error("Error connecting to database: " + err.stack);
-//     return;
-//   }
-//   console.log("Connected to database as id " + connection.threadId);
-//   start();
-// });
-
 const start = function () {
   inquirer.prompt(menu).then((answer) => {
     switch (answer.choice) {
@@ -45,23 +38,22 @@ const start = function () {
         console.log("view departments");
         break;
       case "View all roles":
-        //   viewAllRoles();
+        viewAllRoles();
         console.log("view all roles");
         break;
       case "View all employees":
-        //   viewAllEmployees();
+        viewAllEmployees();
         console.log("view employees");
         break;
       case "Add a department":
-        //   addDepartment();
-        console.log("add dept");
+        addDepartment();
         break;
       case "Add a role":
-        //   addRole();
+        addRole();
         console.log("add role");
         break;
       case "Add an employee":
-        //   addEmployee();
+        addEmployee();
         console.log("add employee");
         break;
       case "Update an employee role":
@@ -76,17 +68,6 @@ const start = function () {
   });
 };
 
-// function viewAllDepartments() {
-//   const query = "SELECT * FROM department";
-
-//   connection
-//     .promise()
-//     .query(query)
-//     .then(([rows, fields]) => {
-//       console.table(rows);
-//     });
-// }
-
 function viewAllDepartments() {
   const query = "SELECT * FROM department";
 
@@ -100,7 +81,121 @@ function viewAllDepartments() {
   });
 }
 
-// start();
+function viewAllRoles() {
+  const query = "SELECT * FROM role";
+
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+
+    console.table(results);
+
+    // Call the inquirer prompt again to show the menu
+    start();
+  });
+}
+
+function viewAllEmployees() {
+  const query = "SELECT * FROM employee";
+
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+
+    console.table(results);
+
+    // Call the inquirer prompt again to show the menu
+    start();
+  });
+}
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "what is the name of the department?",
+      },
+    ])
+    .then((answer) => {
+      const query = "INSERT INTO department (name) VALUES (?)";
+      const values = [answer.name];
+      connection.query(query, values, (err, result) => {
+        if (err) throw err;
+        console.log("department added");
+        start();
+      });
+    });
+}
+
+function addRole() {
+  const query = "SELECT * FROM department";
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "What is the name of the role?",
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "What is the salary of the role?",
+          validate: (input) => {
+            console.log(typeof input);
+            if (isNaN(input)) {
+              return "please enter a valid number";
+            }
+            return true;
+          },
+        },
+        {
+          type: "list",
+          name: "department",
+          message: "Which department does this role belong to?",
+          choices: results.map((department) => department.name),
+        },
+      ])
+      .then((answer) => {
+        const departmentId = results.find(
+          (department) => department.name === answer.department
+        ).id;
+        const query =
+          "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)";
+        const values = [answer.title, answer.salary, departmentId];
+
+        connection.query(query, values, (err, results) => {
+          if (err) throw err;
+
+          console.log("Role added successfully!");
+
+          // Call the inquirer prompt again to show the menu
+          start();
+        });
+      });
+  });
+}
+
+function addEmployee() {
+  const query = "SELECT * FROM role";
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "What is the employees first name?",
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "What is the employees last name?",
+      },
+    ]);
+  });
+}
 
 module.exports = {
   start,
